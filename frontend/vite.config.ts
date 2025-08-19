426d911e0411cd72829e3588e48dca20ba125cd0
Joel Yoder <github@joelyoder.com>
2025-08-18 23:43:29 -0400

    Rename to "Gen Library" and configure Vite for proxied access

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,11 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    proxy: {
+      '/api': 'http://localhost:8081',
+      '/thumbs': 'http://localhost:8081',
+    },
+    host: '0.0.0.0', // <-- this is key
     port: 5174
   }
 })
