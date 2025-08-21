80c15aa6a1b04c0c1888c1f7858c86265ea33ebc
Joel Yoder <github@joelyoder.com>
2025-08-21 13:37:59 -0400

    Fix Lora weight preload query

--- backend/api/images.go
+++ backend/api/images.go
@@ -151,12 +151,20 @@ func listImages(gdb *gorm.DB) gin.HandlerFunc {
 func getImage(gdb *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var m db.Image
-		if err := gdb.Preload("Tags").Preload("Loras", func(db *gorm.DB) *gorm.DB {
-			return db.Select("loras.*, image_loras.weight")
-		}).Preload("Embeddings").Preload("Model").First(&m, c.Param("id")).Error; err != nil {
+		if err := gdb.Preload("Tags").Preload("Embeddings").Preload("Model").First(&m, c.Param("id")).Error; err != nil {
 			c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
 			return
 		}
+		var loras []*db.Lora
+		if err := gdb.Table("loras").
+			Joins("JOIN image_loras ON image_loras.lora_id = loras.id").
+			Select("loras.*, image_loras.weight").
+			Where("image_loras.image_id = ?", m.ID).
+			Find(&loras).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		m.Loras = loras
 		if m.Model != nil {
 			m.ModelName = &m.Model.Name
 			m.ModelHash = m.Model.Hash
