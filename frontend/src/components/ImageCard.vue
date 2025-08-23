362a90c711e10284740f600cba0afbf73b821ba2
Joel Yoder <github@joelyoder.com>
2025-08-23 02:17:12 -0400

    feat: add NSFW toggle button

--- frontend/src/components/ImageCard.vue
+++ frontend/src/components/ImageCard.vue
@@ -1,21 +1,30 @@
 <template>
   <div class="card shadow-sm">
-    <img
-      v-if="image.thumbUrl"
-      :src="apiBase + image.thumbUrl"
-      class="card-img-top"
-      :alt="image.fileName"
-      loading="lazy"
-      @click="onView"
-    />
+    <div class="position-relative">
+      <img
+        v-if="image.thumbUrl"
+        :src="apiBase + image.thumbUrl"
+        class="card-img-top"
+        :alt="image.fileName"
+        loading="lazy"
+        @click="onView"
+      />
+      <button
+        class="btn btn-sm"
+        :class="image.nsfw ? 'btn-danger' : 'btn-outline-danger'"
+        style="position: absolute; top: 0.5rem; right: 0.5rem"
+        @click.stop="onToggleNSFW"
+      >
+        NSFW
+      </button>
+    </div>
     <div class="card-body p-2">
       <div class="d-flex justify-content-between align-items-center">
         <i
           v-if="image.favorite"
           class="bi bi-star-fill text-warning"
         ></i>
-        <div class="d-flex align-items-center gap-1">
-          <span v-if="image.nsfw" class="badge text-bg-danger">NSFW</span>
+        <div class="d-flex align-items-center">
           <button class="btn btn-sm btn-outline-danger" @click="onDelete">
             <i class="bi bi-trash"></i>
           </button>
@@ -26,7 +35,7 @@
 </template>
 
   <script setup lang="ts">
-  import { deleteImage, apiBase } from '../api'
+  import { deleteImage, apiBase, updateImageMetadata } from '../api'
   const props = defineProps<{ image: any }>()
   const emit = defineEmits(['deleted', 'metadata'])
 
@@ -38,5 +47,11 @@
 
   function onView() {
     emit('metadata', props.image)
+  }
+
+  async function onToggleNSFW() {
+    const newVal = !props.image.nsfw
+    await updateImageMetadata(props.image.id, { nsfw: newVal })
+    props.image.nsfw = newVal
   }
 </script>
